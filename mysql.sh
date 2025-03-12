463d2ca0bbebe22f29e2b00c84571817ef952e0f
vinod <mechvnod@gmail.com>
2025-03-12 17:03:36 +0530

    expense-shell-frontend

--- mysql.sh
+++ mysql.sh
@@ -6,14 +6,14 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-LOGS_FOLDER="/var/log/expense-logs"
-LOG_FILE=$(echo $0 | cut -d "." -f1 )
+LOGS_FOLDER="/var/log/expense-log"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
 VALIDATE(){
-    if [ $1 -ne 0 ]
-    then
+    if [ $1 -ne 0]
+     then
         echo -e "$2 ... $R FAILURE $N"
         exit 1
     else
@@ -23,15 +23,18 @@ VALIDATE(){
 
 CHECK_ROOT(){
     if [ $USERID -ne 0 ]
-    then
-        echo "ERROR:: You must have sudo access to execute this script"
-        exit 1 #other than 0
+     then
+      echo -e "$Y You must have sudo access to execute this script $N"
+      exit 1
     fi
 }
-
-echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
+ 
+ 
+echo "Script executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
 
 CHECK_ROOT
+
+
 
 dnf install mysql-server -y &>>$LOG_FILE_NAME
 VALIDATE $? "Installing MySQL Server"
@@ -42,13 +45,13 @@ VALIDATE $? "Enabling MySQL Server"
 systemctl start mysqld &>>$LOG_FILE_NAME
 VALIDATE $? "Starting MySQL Server"
 
-mysql -h mysql.daws82s.online -u root -pExpenseApp@1 -e 'show databases;' &>>$LOG_FILE_NAME
+mysql -h mysql.vinodshetty.online -u root -p<password> -e 'show databases;' &>>$LOG_FILE_NAME
 
-if [ $? -ne 0 ]
-then
-    echo "MySQL Root password not setup" &>>$LOG_FILE_NAME
+if [ $? -ne 0]
+ then
+    echo "MySQL root password not setup" &>>$LOG_FILE_NAME
     mysql_secure_installation --set-root-pass ExpenseApp@1
-    VALIDATE $? "Setting Root Password"
+    VALIDATE $? "Setting root password"
 else
-    echo -e "MySQL Root password already setup ... $Y SKIPPING $N"
+    echo "MySQL root password already setup ... $Y SKIPPING $N"
 fi
